1b113aa40662a920e095cd0cb100deca16dade42
brokans <46932664+brokans@users.noreply.github.com>
2023-09-26 12:24:51 +0300

    player score save

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -50,28 +50,62 @@ function App() {
   //   setPlayers(newGame);
   // }
 
-  function addPlayers() {
-    const playerOne = nameRefOne.current.value;
-    const playerTwo = nameRefTwo.current.value;
+  // function addPlayers() {
+  //   const playerOne = nameRefOne.current.value;
+  //   const playerTwo = nameRefTwo.current.value;
 
-    const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
-    const gamesPlayed = gamesData.find((game) => game.playerOne === playerOne && game.playerTwo === playerTwo);
-    if (gamesPlayed){
-      setPlayers(gamesPlayed);
-    } else {
-      const newGame = {
-        playerOne,
-        playerTwo,
-        scores: { playerOneScore: 0, playerTwoScore: 0 },
-      };
-      gamesData.push(newGame);
+  //   const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+  //   const gamesPlayed = gamesData.find((game) => game.playerOne === playerOne && game.playerTwo === playerTwo);
+  //   if (gamesPlayed){
+  //     setPlayers(gamesPlayed);
+  //   } else {
+  //     const newGame = {
+  //       playerOne,
+  //       playerTwo,
+  //       scores: { playerOneScore: 0, playerTwoScore: 0 },
+  //     };
+  //     gamesData.push(newGame);
   
-      localStorage.setItem("games", JSON.stringify(gamesData));
-      setMessage("Have a good Game!");
-      setPlayers(newGame);
-    }
+  //     localStorage.setItem("games", JSON.stringify(gamesData));
+  //     setMessage("Have a good Game!");
+  //     setPlayers(newGame);
+  //   }
     
+  // }
+
+  function addPlayers() {
+  const playerOne = nameRefOne.current.value;
+  const playerTwo = nameRefTwo.current.value;
+
+  const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+  const gamesPlayed = gamesData.find(
+    (game) => game.playerOne === playerOne && game.playerTwo === playerTwo
+  );
+
+  if (gamesPlayed) {
+    // Players have played before, set their scores from previous games
+    setPlayers(gamesPlayed);
+    setScores(gamesPlayed.scores);
+  } else {
+    const newGame = {
+      playerOne,
+      playerTwo,
+      scores: { playerOneScore: 0, playerTwoScore: 0 },
+    };
+    gamesData.push(newGame);
+
+    localStorage.setItem("games", JSON.stringify(gamesData));
+
+    // Reset the scores in the state
+    setScores({
+      playerOneScore: 0,
+      playerTwoScore: 0,
+    });
+
+    setMessage("Have a good Game!");
+    setPlayers(newGame);
   }
+}
 
 
   const WIN_CONDITIONS = [
@@ -92,6 +126,38 @@ function App() {
   //   return sum;
   // }
 
+  // const handleBoxClick = (boxIdx) => {
+  //   // Step 1: Update the board
+  //   const updatedBoard = board.map((value, idx) => {
+  //     if (idx === boxIdx) {
+  //       return xPlaying ? "X" : "O";
+  //     } else {
+  //       return value;
+  //     }
+  //   });
+
+
+  //   setBoard(updatedBoard);
+
+  //   // Step 2: Check if either player has won the game
+  //   const winner = checkWinner(updatedBoard);
+
+  //   if (winner) {
+  //     if (winner === "O") {
+  //       let { playerTwoScore } = scores;
+  //       playerTwoScore += 1;
+  //       setScores({ ...scores, playerTwoScore });
+  //     } else {
+  //       let { playerOneScore } = scores;
+  //       playerOneScore += 1;
+  //       setScores({ ...scores, playerOneScore });
+  //     }
+  //   }
+
+  //   // Step 3: Change active player
+  //   setXPlaying(!xPlaying);
+  // };
+
   const handleBoxClick = (boxIdx) => {
     // Step 1: Update the board
     const updatedBoard = board.map((value, idx) => {
@@ -101,28 +167,53 @@ function App() {
         return value;
       }
     });
-
-
+  
     setBoard(updatedBoard);
-
+  
     // Step 2: Check if either player has won the game
     const winner = checkWinner(updatedBoard);
-
+  
     if (winner) {
       if (winner === "O") {
         let { playerTwoScore } = scores;
         playerTwoScore += 1;
         setScores({ ...scores, playerTwoScore });
+  
+        // Save the updated scores to local storage
+        const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+        const currentPlayer = gamesData.find(
+          (game) =>
+            game.playerOne === players.playerOne &&
+            game.playerTwo === players.playerTwo
+        );
+        if (currentPlayer) {
+          currentPlayer.scores.playerTwoScore = playerTwoScore;
+          localStorage.setItem("games", JSON.stringify(gamesData));
+        }
       } else {
         let { playerOneScore } = scores;
         playerOneScore += 1;
         setScores({ ...scores, playerOneScore });
+  
+        // Save the updated scores to local storage
+        const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+        const currentPlayer = gamesData.find(
+          (game) =>
+            game.playerOne === players.playerOne &&
+            game.playerTwo === players.playerTwo
+        );
+        if (currentPlayer) {
+          currentPlayer.scores.playerOneScore = playerOneScore;
+          localStorage.setItem("games", JSON.stringify(gamesData));
+        }
       }
     }
-
+  
     // Step 3: Change active player
     setXPlaying(!xPlaying);
   };
+  
+
 
   const checkWinner = (board) => {
     for (let i = 0; i < WIN_CONDITIONS.length; i++) {
@@ -141,9 +232,6 @@ function App() {
     setBoard(Array(9).fill(null));
   };
 
-  // if (!game) {
-  //   return <div>Loading...</div>; // You can add a loading state while data is being fetched
-  // }
 
   // vaja saata ainult sisestatud mängijate nimed Game lehele
 
--- tic-tac-toe/src/components/ScoreBoard.jsx
+++ tic-tac-toe/src/components/ScoreBoard.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react'
 import { GameContext } from "../store/GameContext";
 // import { useGameContext } from "../store/GameContext";
-
-
 import "./ScoreBoard.css"
 
 export const ScoreBoard = ({ scores, xPlaying }) => {
@@ -20,3 +18,70 @@ export const ScoreBoard = ({ scores, xPlaying }) => {
     </div>
   )
 }
+
+// function addPlayers() {
+//   const playerOne = nameRefOne.current.value;
+//   const playerTwo = nameRefTwo.current.value;
+
+//   const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+//   const gamesPlayed = gamesData.find(
+//     (game) => game.playerOne === playerOne && game.playerTwo === playerTwo
+//   );
+
+//   if (gamesPlayed) {
+//     setPlayers(gamesPlayed);
+//   } else {
+//     const newGame = {
+//       playerOne,
+//       playerTwo,
+//       scores: { playerOneScore: 0, playerTwoScore: 0 },
+//     };
+//     gamesData.push(newGame);
+
+//     localStorage.setItem("games", JSON.stringify(gamesData));
+
+//     // Reset the scores in the state
+//     setScores({
+//       playerOneScore: 0,
+//       playerTwoScore: 0,
+//     });
+
+//     setMessage("Have a good Game!");
+//     setPlayers(newGame);
+//   }
+// }
+
+// function addPlayers() {
+//   const playerOne = nameRefOne.current.value;
+//   const playerTwo = nameRefTwo.current.value;
+
+//   const gamesData = JSON.parse(localStorage.getItem("games") || "[]");
+//   const gamesPlayed = gamesData.find(
+//     (game) => game.playerOne === playerOne && game.playerTwo === playerTwo
+//   );
+
+//   if (gamesPlayed) {
+//     // Players have played before, set their scores from previous games
+//     setPlayers(gamesPlayed);
+//     setScores(gamesPlayed.scores);
+//   } else {
+//     const newGame = {
+//       playerOne,
+//       playerTwo,
+//       scores: { playerOneScore: 0, playerTwoScore: 0 },
+//     };
+//     gamesData.push(newGame);
+
+//     localStorage.setItem("games", JSON.stringify(gamesData));
+
+//     // Reset the scores in the state
+//     setScores({
+//       playerOneScore: 0,
+//       playerTwoScore: 0,
+//     });
+
+//     setMessage("Have a good Game!");
+//     setPlayers(newGame);
+//   }
+// }
+
