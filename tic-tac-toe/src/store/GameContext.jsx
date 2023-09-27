b86818a2c2d2d504def425ff9af9b1decb56f3d2
brokans <46932664+brokans@users.noreply.github.com>
2023-09-27 21:29:37 +0300

    firebase upload

--- tic-tac-toe/src/store/GameContext.jsx
+++ tic-tac-toe/src/store/GameContext.jsx
@@ -1,27 +1,12 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const GameContext = createContext();
 
-
-// siin peaks toimuma mängijate tulemuste võrdlemine
-//GPT
-// export const useGameContext = () => useContext(GameContext);
-
-
-
 export function GameContextProvider({ children }) {
-  // GPT
-  // const [gamesHistory, setGamesHistory] = useState([]);
   const initialPlayers = JSON.parse(localStorage.getItem("games") || "[]");
 
-  const [players, setPlayers] = useState(initialPlayers)
+  const [players, setPlayers] = useState(initialPlayers);
 
-  //GPT
-  // const addGameToHistory = (newGame) => {
-  //   setGamesHistory((prevHistory) => [...prevHistory, newGame]);
-  // };
-  
-  
   return (
     <GameContext.Provider value={{ players, setPlayers }}>
       {children}
